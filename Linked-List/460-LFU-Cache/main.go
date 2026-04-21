dc1e55f73aae565478b7a48e99e0a8a8a3a29200
weishun Huang <water25234@gmail.com>
2026-04-21 22:02:36 +0800

    extra description

--- Linked-List/460-LFU-Cache/main.go
+++ Linked-List/460-LFU-Cache/main.go
@@ -49,6 +49,29 @@ package main
 // 0 <= value <= 109
 // At most 2 * 105 calls will be made to get and put.
 
+// type LFUCache struct {
+
+// }
+
+// func Constructor(capacity int) LFUCache {
+
+// }
+
+// func (this *LFUCache) Get(key int) int {
+
+// }
+
+// func (this *LFUCache) Put(key int, value int)  {
+
+// }
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
+
 type Node struct {
 	key  int
 	val  int
@@ -107,6 +130,7 @@ type LFUCache struct {
 	freqToList map[int]*DoublyLinkedList
 }
 
+// main
 func Constructor(capacity int) LFUCache {
 	return LFUCache{
 		capacity:   capacity,
